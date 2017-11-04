634c69671fe5bced2238a3994ed7ec1e9905b6e0
rahul <rahulsingh2240@gmail.com>
2017-11-04 11:43:44 +0530

    working with urls

--- greetings/urls.py
+++ greetings/urls.py
@@ -4,5 +4,5 @@ from .views import index
 
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^hello$', index),
 ]
--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -18,8 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
-
-urlpatterns = [
     url(r'^', include('greetings.urls')),
 ]
